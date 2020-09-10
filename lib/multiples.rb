c2c011b67a1b981a3547d21f8207e4b7dedb6bdb
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-10 16:23:39 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  array = (1...limit)
+  return array.find_all {|num| num%3 == 0 || num%5 ==0}
+end 
+
+def sum_multiples(limit)
+  return collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit 
+
+  def initialize(limit)
+    @limit=limit
+  end 
+
+  def collect_multiples
+    array = (1...limit)
+    array.find_all {|num| num%3 == 0 || num%5 ==0}
+  end 
+  
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+
+end
